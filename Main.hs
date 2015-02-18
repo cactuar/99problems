d96b2fc64f5607dfadbce2e43c703c6f5aec500e
Marco Sormani <marco.sormani@gmail.com>
2015-02-18 12:46:42 +0100

    Added 10

--- Main.hs
+++ Main.hs
@@ -41,3 +41,18 @@ myflatten (List x) = concatMap myflatten x
 -- 08
 compress :: Eq a => [a] -> [a]
 compress (y:ys) = foldl (\a x -> if x == last a then a else a ++ [x]) [y] ys
+
+-- compress' (y:ys) = foldr (\a x -> if x == head a then a else x:a) [y] ys
+
+-- 09
+pack :: Eq a => [a] -> [[a]]
+pack [] = []
+pack (y:ys) = foldl (\a x -> if x /= head(last a)
+                             then a ++ [[x]]
+                             else let i = init a
+                                      l = last a in i ++ [l ++ [x]])
+              [[y]] ys
+
+-- 10
+encode :: Eq a => [a] -> [(Int, a)]
+encode y = foldr (\x a -> (length x, head x):a) [] $ pack y
